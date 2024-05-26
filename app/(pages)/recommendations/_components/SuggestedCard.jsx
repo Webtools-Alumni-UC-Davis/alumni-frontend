e5c42e109a1bf526b4617e9749cf9f364cf51818
Japman Singh Kohli <91647755+buzzshocker@users.noreply.github.com>
2024-05-25 17:20:20 -0700

    Update SuggestedCard.jsx

--- app/(pages)/recommendations/_components/SuggestedCard.jsx
+++ app/(pages)/recommendations/_components/SuggestedCard.jsx
@@ -1,12 +1,11 @@
 'use client';
 import React, { useState, useEffect, useRef } from 'react';
-import Chart from 'chart.js/auto';
 import styles from '@components/Card/Card.module.scss';
 import layout from '@components/Layout/Layout.module.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export default function SuggestedCard() {
     const [companyData, setCompanyData] = useState(null);
-    const chartRefs = useRef({});
 
     useEffect(() => {
         fetchCompanyData();
@@ -15,7 +14,7 @@ export default function SuggestedCard() {
     const fetchCompanyData = async () => {
         try {
             const response = await fetch(
-                "https://alumni-backend-6954.onrender.com/equity-zen/"
+                "http://localhost:3002/equity-zen/"
             );
             if (!response.ok) {
                 throw new Error('Failed to fetch company data');
@@ -27,79 +26,6 @@ export default function SuggestedCard() {
         }
     };
 
-    useEffect(() => {
-        if (companyData) {
-            renderFundingCharts();
-        }
-    }, [companyData]);
-
-    const renderFundingCharts = () => {
-      companyData.forEach(company => {
-          const fundingByYear = {};
-          company.fundingRecord.forEach(record => {
-              const year = record.period.split(' ')[1];
-              if (!fundingByYear[year]) {
-                  fundingByYear[year] = 0;
-              }
-              fundingByYear[year] += parseFloat(record.amount.replace('$', '').replace('M', '')) * 1000000;
-          });
-  
-          const years = Object.keys(fundingByYear);
-          const amounts = Object.values(fundingByYear);
-  
-          const canvasId = `fundingChart_${company._id}`;
-          const ctx = chartRefs.current[company._id]?.getContext('2d');
-          if (ctx) {
-              Chart.getChart(ctx)?.destroy();
-          }
-          chartRefs.current[company._id] = ctx;
-  
-          new Chart(ctx, {
-              type: 'line',
-              data: {
-                  labels: years,
-                  datasets: [{
-                      label: 'Funding History',
-                      data: amounts,
-                      fill: false,
-                      borderColor: 'rgb(154, 173, 194)',
-                      tension: 0.1,
-                      borderWidth: 1
-                  }]
-              },
-              options: {
-                scales: {
-                  y: {
-                    beginAtZero: true,
-                    display: true,
-                    color: '#FFFFFF',
-                    grid: {
-                      color: '#FFFFFF'
-                    },
-                    ticks: {
-                      color: '#FFFFFF' 
-                    }
-                  },
-                  x: {
-                    display: true,
-                    grid: {
-                      color: '#FFFFFF'
-                    },
-                    ticks: {
-                      display: false, 
-                    },
-                  }
-                },
-                plugins: {
-                  legend: {
-                    display: false,
-                  }
-                }
-              }
-          });
-      });
-  };
-  
 
     if (!companyData) {
         return <div>Loading...</div>;
@@ -107,27 +33,25 @@ export default function SuggestedCard() {
 
     return (
         <div className={styles.RowCol}>
-          <h4> Suggested Companies with Davis Alumnis </h4>
             {companyData.map(company => (
                 <div key={company._id} className={layout.ColRow}>
-                    <div className={styles.black}>
-                        <h4>{company.name}</h4>
-                        <span className={styles.line}></span>
-                        <div className={layout.align}> 
-                          <canvas id={`fundingChart_${company._id}`} width="400" height="150" ref={(el) => (chartRefs.current[company._id] = el)} />
-                          <div className={styles.row}>
-                            <div style={{ backgroundColor: 'rgb(154, 173, 194)', width: '8px', height: '8px', display: 'inline-block', 
-                            marginRight: '5px', borderRadius: '15px' }}></div>
-                            <p>Funding History</p>
-                          </div>
-                        </div>
-                    </div>
                     <div className={styles.lightblue}>
+                        <h3>
+                            <a href={company.ezenLink} target="_blank" rel="noopener noreferrer">
+                                {company.name}
+                            </a>
+                        </h3>
                         <p>{company.bio}</p>
                         <p><span className={styles.underline}>Founded:</span> {company.foundingDate}</p>
                         <p><span className={styles.underline}>Notable Investors:</span> {company.notableInvestors}</p>
                         <p><span className={styles.underline}>Headquarters:</span> {company.hq}</p>
                         <p><span className={styles.underline}>Total Funding:</span> {company.totalFunding}</p>
+                        <p className={styles.bold}>Industries:</p>
+                        {company.industries && company.industries.length > 0 ? (
+                            <p>{company.industries.join(', ')}</p>
+                        ) : (
+                            <p>No industry information available.</p>
+                        )}
                         <p className={styles.bold}>Management:</p>
                         {company.founders ? (
                             company.founders.map(founder => (
@@ -136,14 +60,33 @@ export default function SuggestedCard() {
                         ) : (
                             <p>No management information available.</p>
                         )}
-                        <p className={styles.bold}>UC Davis Alumni within the company:</p>
+                    </div>
+                    <div className={styles.black}>
+                        <h4 className={styles.bold}>UC Davis Alumni within the company:</h4>
+                        <span className={styles.line}></span>
                         {company.alumnis !== null ? (
                             company.alumnis.map(alumni => (
-                                <p key={alumni._id}>{alumni.name}</p>
+                                <p key={alumni._id}><a href={alumni.url} target="_blank" rel="noopener noreferrer">
+                                    {alumni.name} - {alumni.position}
+                                </a> </p>
                             ))
                         ) : (
                             <p>No UC Davis Alumni found within the company.</p>
                         )}
+                        <div className={layout.align}>
+                            <canvas width="400" height="150" />
+                            <div className={styles.row}>
+                                <div style={{ backgroundColor: 'rgb(154, 173, 194)', marginRight: '5px'}}></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.RowCol}>
+                        <div className={`${styles.button} ${styles.favorite}`}>
+                            <p>Favorite</p>
+                        </div>
+                        <div className={`${styles.button} ${styles.delete}`}>
+                            <i className="fa-solid fa-trash"></i>
+                        </div>
                     </div>
                 </div>
             ))}
