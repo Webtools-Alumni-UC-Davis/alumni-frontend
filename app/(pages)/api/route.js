44e3f3b67ee11a7423663e2974700b2040dbf808
Yash <yinani@ucdavis.edu>
2024-05-31 17:36:29 -0700

    user_id capture

--- app/(pages)/api/route.js
+++ app/(pages)/api/route.js
@@ -1,4 +1,10 @@
 export async function GET(request) {
+    const headers = request.headers;
+
+    // Log each header
+    for (const [key, value] of headers.entries()) {
+        console.log(`${key}: ${value}`);
+    }
     const userId = request.headers["REMOTE_USER"];
     console.log(userId);
     return Response.json({ userId });
