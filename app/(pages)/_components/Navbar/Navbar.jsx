e36376ee4eba53f8944fb1b843ac6b15c43a5972
Yash <yinani@ucdavis.edu>
2024-06-03 01:20:53 -0700

    updates in email id's and name

--- app/(pages)/_components/Navbar/Navbar.jsx
+++ app/(pages)/_components/Navbar/Navbar.jsx
@@ -15,48 +15,56 @@ export default function Navbar({ navLinks }) {
   }, {});
 
   return (
-    <div className={styles.relative_wrapper} data-testid="navbar">
-      <div className={styles.container}>
-        <div className={styles.hello}>
-          <h2> Erin</h2>
-          <h4 className={styles.email}>erin@email.com</h4>{" "}
-        </div>
-        <div className={styles.nav_container}>
-          <div className={`${styles.links} ${active ? styles.active : null}`}>
-            {Object.entries(groupedLinks).map(([type, links]) => (
-              <div key={type}>
-                <h4 className={styles.type}>{type}</h4>
-                {links.map((link) => (
-                  <Link key={link.slug} href={link.slug}>
-                    <div className={styles.link}>
-                      {link.image && (
-                        <Image
-                          className={styles.linkImage}
-                          src={link.image}
-                          width={30}
-                          height={30}
-                          alt={link.name}
-                        />
-                      )}
-                      {link.name}
-                    </div>
-                  </Link>
-                ))}
+      <div className={styles.relative_wrapper} data-testid="navbar">
+          <div className={styles.container}>
+              <div className={styles.hello}>
+                  <h3>Erin Friscia</h3>
+                  <h4 className={styles.email}>eofriscia@ucdavis.edu</h4>{" "}
               </div>
-            ))}
+              <div className={styles.nav_container}>
+                  <div
+                      className={`${styles.links} ${
+                          active ? styles.active : null
+                      }`}
+                  >
+                      {Object.entries(groupedLinks).map(([type, links]) => (
+                          <div key={type}>
+                              <h4 className={styles.type}>{type}</h4>
+                              {links.map((link) => (
+                                  <Link key={link.slug} href={link.slug}>
+                                      <div className={styles.link}>
+                                          {link.image && (
+                                              <Image
+                                                  className={styles.linkImage}
+                                                  src={link.image}
+                                                  width={30}
+                                                  height={30}
+                                                  alt={link.name}
+                                              />
+                                          )}
+                                          {link.name}
+                                      </div>
+                                  </Link>
+                              ))}
+                          </div>
+                      ))}
+                  </div>
+                  <button
+                      className={styles.menu}
+                      onClick={toggleActive}
+                      data-testid="menu-button"
+                  >
+                      {active ? <RxCross2 /> : <RxHamburgerMenu />}
+                  </button>
+              </div>
+              <Image
+                  className={styles.Webtool}
+                  src="/index/webtool.png"
+                  width={200}
+                  height={100}
+                  alt="Logo"
+              />
           </div>
-          <button className={styles.menu} onClick={toggleActive} data-testid="menu-button">
-            {active ? <RxCross2 /> : <RxHamburgerMenu />}
-          </button>
-        </div>
-        <Image
-          className={styles.Webtool}
-          src="/index/webtool.png"
-          width={200}
-          height={100}
-          alt="Logo"
-        />
       </div>
-    </div>
   );
 }
--- app/(pages)/api/route.js
+++ app/(pages)/api/route.js
@@ -2,11 +2,11 @@ export async function GET(request) {
     const headers = request.headers;
 
     // Log each header
-    for (const [key, value] of headers.entries()) {
-        console.log(`${key}: ${value}`);
-    }
+    // for (const [key, value] of headers.entries()) {
+    //     console.log(`${key}: ${value}`);
+    // }
     const userId = request.headers["REMOTE_USER"];
-    console.log(userId);
+    // console.log(userId);
     return Response.json({ userId });
 }
 
